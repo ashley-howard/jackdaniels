32fc31504afab66e686d4a3091e25b60ced1264d
noteconvert <me@ashley.how>
2019-08-24 14:57:41 +0200

    add weather JS

--- scripts/index.js
+++ scripts/index.js
@@ -1,24 +1,62 @@
 const appKey = "6a42348f3f2db5f744296408f0807ef2";
-let weatherLocation = "London"; // or you could take it from the innerHTML on the homepage?
+const weatherUnit = "&units=metric"; // or "imperial" for fahrenheit
+let weatherLocation = document.getElementById("weather-location").innerHTML; // getting location from HTML
 let weatherIcon = document.getElementById("weather-icon");
+let temperature = document.getElementById("weather-temp");
+let weatherUpdate = document.getElementById("update-weather")
 
-let api = "https://api.openweathermap.org/data/2.5/weather?q=" + weatherLocation + "&appid=" + appKey;
+let api = "https://api.openweathermap.org/data/2.5/weather?q=" + weatherLocation + "&appid=" + appKey + weatherUnit;
+
+// https://openweathermap.org/weather-conditions
+// clear <i class="fas fa-sun"></i>
+// clouds <i class="fas fa-cloud"></i>
+// rain <i class="fas fa-cloud-rain"></i>
+// thunder <i class="fas fa-bolt"></i>
+// snow <i class="fas fa-snowflake"></i>
+// mist <i class="fas fa-smog"></i>
+
+
+// On load > check if weather is set
+//           > 1) if not, load it and save (also save current time)
+//           > 2) if set, check if 1 hour has passed
+//                        > if it has, repeat 1)
+//                        > if not, don't do anything
+
+
 
 function findWeatherDetails() {
-    api;
-    httpRequestAsync(searchLink, theResponse);
+    httpRequestAsync(api, theResponse);
 }
 
 function theResponse(response) {
     let jsonObject = JSON.parse(response);
-    cityName.innerHTML = jsonObject.name;
-    icon.src = "http://openweathermap.org/img/w/" + jsonObject.weather[0].icon + ".png";
-    temperature.innerHTML = parseInt(jsonObject.main.temp - 273) + "°";
-    humidity.innerHTML = jsonObject.main.humidity + "%";
+    temperature.innerHTML = parseInt(jsonObject.main.temp) + "°";
+
+    if (200 <= jsonObject.weather[0].id && jsonObject.weather[0].id <= 232) {
+        weatherIcon.className = "fas fa-bolt";
+    }
+    else if (300 <= jsonObject.weather[0].id && jsonObject.weather[0].id <= 531) {
+        weatherIcon.className = "fas fa-cloud-rain";
+    }
+    else if (600 <= jsonObject.weather[0].id && jsonObject.weather[0].id <= 622) {
+        weatherIcon.className = "fas fa-snowflake";
+    }
+    else if (701 <= jsonObject.weather[0].id && jsonObject.weather[0].id <= 781) {
+        weatherIcon.className = "fas fa-smog";
+    }
+    else if (jsonObject.weather[0].id && jsonObject.weather[0].id === 800) {
+        weatherIcon.className = "fas fa-sun";
+    }
+    else if (801 <= jsonObject.weather[0].id && jsonObject.weather[0].id <= 804) {
+        weatherIcon.className = "fas fa-cloud";
+    }
+    else {
+        console.log("Icon error");
+    }
+
 }
 
 function httpRequestAsync(url, callback) {
-    console.log("hello");
     var httpRequest = new XMLHttpRequest();
     httpRequest.onreadystatechange = () => {
         if (httpRequest.readyState == 4 && httpRequest.status == 200)
@@ -27,17 +65,3 @@ function httpRequestAsync(url, callback) {
     httpRequest.open("GET", url, true); // true for asynchronous 
     httpRequest.send();
 }
-
-const homeScreen = document.getElementById("home");
-const contactScreen = document.getElementById("contact");
-const screenButton = document.getElementById("screenbutton");
-
-function changeScreen(screen) {
-    if (screen === "contact") {
-        homeScreen.style.display = "none";
-        contactScreen.style.display = "block";
-        contactScreen.style.WebkitTransition = "all 2s"; // Code for Safari 3.1 to 6.0
-        contactScreen.style.transition = "all 2s";     // Standard syntax
-    }
-}
-
